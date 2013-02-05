319dfdb8940b132804d86c2bd401b01293d81115
Chad Strauch <boatski313@gmail.com>
2013-02-04 21:28:48 -0500

    added grid to resizable/draggable

--- js/main.js
+++ js/main.js
@@ -10,14 +10,16 @@
 		$('.resizable').draggable({
 			appendTo: 'body',
 			containment: '.not-resizable',
-			axis: 'y'});
+			axis: 'y',
+			grid: [15,15]});
 
 		$('.resizable').resizable({
 			handles: 'n,s', 
 			ghost: false, 
 			animate: false,
 			maxHeight: 360,
-			minHeight: 60});
+			minHeight: 60,
+			grid: [15,15]});
 	}
  
  
