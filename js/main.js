d936f2f81a67eddf4680a5990c05a63ca920ac8f
Chad Strauch <boatski313@gmail.com>
2013-02-05 01:06:31 -0500

    more progress on scheduling design

--- js/main.js
+++ js/main.js
@@ -6,21 +6,47 @@
 
  	function setDraggableAndResizable()
  	{
+ 		var width = $('.resizable').width();
+ 		console.log(width);
 
 		$('.resizable').draggable({
 			appendTo: 'body',
-			containment: '.not-resizable',
+			containment: 'parent',
 			axis: 'y',
-			grid: [15,15]});
+			grid: [15,15],
+			drag: function(event, ui)
+			{
+				updateTimes(this);
+			}});
 
 		$('.resizable').resizable({
 			handles: 'n,s', 
+			containment: 'parent',
 			ghost: false, 
 			animate: false,
 			maxHeight: 360,
 			minHeight: 60,
-			grid: [15,15]});
-	}
+			grid: [15,15],
+			start: function(event, ui)
+			{
+				width = $('.resizable').width();
+			},
+			resize: function(event, ui)
+			{
+				updateTimes(this);
+				$('.resizable').css('width', width);
+			},
+			stop: function(event, ui)
+			{
+				$('.resizable').css('width', width);
+			}});
+	}// end setDraggableAndResizable
+
+	function updateTimes(bar)
+	{
+		var barDay = $(bar).attr('id');
+		console.log(barDay);
+	}// end updateTimes(bar)
  
  
  });
