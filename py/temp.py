bd8621e595c646d6ea87dc23043ebb2761059c0d
Chad Strauch <boatski313@gmail.com>
2013-02-15 00:47:32 -0500

    temp python script

--- py/temp.py
+++ py/temp.py
@@ -1,12 +1,19 @@
 import subprocess
 import ast
 
-
-process = subprocess.Popen(['../Adafruit_DHT', '11', '4'], stdout=subprocess.PIPE)
-out, err = process.communicate()
-
-out = "{" + out + "}"
+""" 
+Runs the C program to poll the DHT 11 sensor.
+Output is then parsed into a dictionary
+"""
+#process = subprocess.Popen(['../Adafruit_DHT', '11', '4'], stdout=subprocess.PIPE)
+#out, err = process.communicate()
+temp = "'Temp' : 72, 'Hum' : 35"
+out = "{" + temp + "}"
 
 data = ast.literal_eval(out)
 
-print("Temperature: " + str(data['Temp']))
+temperature = data['Temp']
+humidity = data['Hum']
+
+print("Temperature: " + str(temperature))
+print("Humidity: " + str(humidity))
