447a10e84bc922538e1838f382302662663cd831
Chad Strauch <boatski313@gmail.com>
2013-02-21 19:22:24 -0500

    formatting changes

--- schedule.php
+++ schedule.php
@@ -50,7 +50,6 @@
 <body>
 
 
-
   <div class="row">
     <div class="twelve columns">
       <h2>Pi-Stat</h2>
@@ -62,149 +61,168 @@
   		</ul>
     </div>
   </div>
+  <div class="test"></div>
+  <form method="POST" action="ajax/updateSchedule.php" id="schedule">
+    <div class="row collapse">
+      <div class="twelve columns centered">
 
-  <div class="row">
-    <div class="twelve columns">
-
-      <div class="row collapse">
-        <div class="date-headers offset-by-one one columns">
-          Mon
-        </div>
-
-        <div class="date-headers one columns">
-          Tues
-        </div>
-
-        <div class="date-headers one columns">
-          Weds
-        </div>
-
-        <div class="date-headers one columns">
-          Thurs
-        </div>
-
-        <div class="date-headers one columns">
-          Fri
-        </div>
-
-        <div class="date-headers one columns">
-          Sat
-        </div>
-
-        <div class="date-headers one columns end">
-          Sun
-        </div>
-      </div><!-- end row -->
-      
-      <div class="row collapse">
-        <div class="schedule-times one columns">
-          <ul>
-            <li>3:00am</li>
-            <li>6:00am</li>
-            <li>9:00am</li>
-            <li>12:00pm</li>
-            <li>3:00pm</li>
-            <li>6:00pm</li>
-            <li>9:00pm</li>
-        </div>
-        <div class="not-resizable one columns">
-          <div class="resizable" id="monday">
+        <div class="row collapse">
+          <div class="date-headers one columns">
+            <!-- empty, offset-by-one doesn't add margin-right -->
           </div>
-        </div><!-- Monday -->
 
-        <div class="not-resizable one columns">
-          <div class="resizable" id="tuesday">
+          <div class="date-headers one columns">
+            Mon
           </div>
-        </div><!-- Tuesday -->
 
-        <div class="not-resizable one columns">
-          <div class="resizable" id="wednesday">
+          <div class="date-headers one columns">
+            Tues
           </div>
-        </div><!-- Wednesday -->
 
-        <div class="not-resizable one columns">
-          <div class="resizable" id="thursday">
+          <div class="date-headers one columns">
+            Weds
           </div>
-        </div><!-- Thursday -->
 
-        <div class="not-resizable one columns">
-          <div class="resizable" id="friday">
+          <div class="date-headers one columns">
+            Thurs
           </div>
-        </div><!-- Friday -->
 
-        <div class="not-resizable one columns">
-          <div class="resizable" id="saturday">
+          <div class="date-headers one columns">
+            Fri
           </div>
-        </div><!-- Saturday -->
 
-        <div class="not-resizable one columns end">
-          <div class="resizable" id="sunday">
+          <div class="date-headers one columns">
+            Sat
           </div>
-        </div><!-- Sunday -->
-      </div><!-- end row -->
 
-      <div class="row collapse">
-        <div class="date-headers one columns offset-by-one">
-          <span id="monday-start-times">7:00</span><span id="monday-start-period">am</span>
-        </div>
+          <div class="date-headers one columns end">
+            Sun
+          </div>
+        </div><!-- end row -->
+        
+        <div class="row collapse">
+          <div class="schedule-times one columns">
+            <ul>
+              <li>3:00am</li>
+              <li>6:00am</li>
+              <li>9:00am</li>
+              <li>12:00pm</li>
+              <li>3:00pm</li>
+              <li>6:00pm</li>
+              <li>9:00pm</li>
+          </div>
+          <div class="not-resizable one columns">
+            <div class="resizable" id="monday">
+            </div>
+          </div><!-- Monday -->
 
-        <div class="date-headers one columns">
-          <span id="tuesday-start-times">7:00</span><span id="tuesday-start-period">am</span>
-        </div>
+          <div class="not-resizable one columns">
+            <div class="resizable" id="tuesday">
+            </div>
+          </div><!-- Tuesday -->
 
-        <div class="date-headers one columns">
-          <span id="wednesday-start-times">7:00</span><span id="wednesday-start-period">am</span>
-        </div>
+          <div class="not-resizable one columns">
+            <div class="resizable" id="wednesday">
+            </div>
+          </div><!-- Wednesday -->
 
-        <div class="date-headers one columns">
-          <span id="thursday-start-times">7:00</span><span id="thursday-start-period">am</span>
-        </div>
+          <div class="not-resizable one columns">
+            <div class="resizable" id="thursday">
+            </div>
+          </div><!-- Thursday -->
 
-        <div class="date-headers one columns">
-          <span id="friday-start-times">7:00</span><span id="friday-start-period">am</span>
-        </div>
+          <div class="not-resizable one columns">
+            <div class="resizable" id="friday">
+            </div>
+          </div><!-- Friday -->
 
-        <div class="date-headers one columns">
-          <span id="saturday-start-times">7:00</span><span id="saturday-start-period">am</span>
-        </div>
+          <div class="not-resizable one columns">
+            <div class="resizable" id="saturday">
+            </div>
+          </div><!-- Saturday -->
 
-        <div class="date-headers one columns end">
-          <span id="sunday-start-times">7:00</span><span id="sunday-start-period">am</span>
-        </div>
-      </div><!-- end row -->
+          <div class="not-resizable one columns end">
+            <div class="resizable" id="sunday">
+            </div>
+          </div><!-- Sunday -->
+        </div><!-- end row -->
 
-      <div class="row collapse">
-        <div class="date-headers one columns offset-by-one">
-          <span id="monday-stop-times">7:00</span><span id="monday-stop-period">pm</span>
-        </div>
+        <div class="row collapse">
+          <div class="time-footers one columns">
 
-        <div class="date-headers one columns">
-          <span id="tuesday-stop-times">7:00</span><span id="tuesday-stop-period">pm</span>
-        </div>
+          </div>
 
-        <div class="date-headers one columns">
-          <span id="wednesday-stop-times">7:00</span><span id="wednesday-stop-period">pm</span>
-        </div>
+          <div class="time-footers one columns">
+            <span id="monday-start-times">7:00</span><span id="monday-start-period">am</span>
+          </div>
 
-        <div class="date-headers one columns">
-          <span id="thursday-stop-times">7:00</span><span id="thursday-stop-period">pm</span>
-        </div>
+          <div class="time-footers one columns">
+            <span id="tuesday-start-times">7:00</span><span id="tuesday-start-period">am</span>
+          </div>
 
-        <div class="date-headers one columns">
-          <span id="friday-stop-times">7:00</span><span id="friday-stop-period">pm</span>
-        </div>
+          <div class="time-footers one columns">
+            <span id="wednesday-start-times">7:00</span><span id="wednesday-start-period">am</span>
+          </div>
 
-        <div class="date-headers one columns">
-          <span id="saturday-stop-times">7:00</span><span id="saturday-stop-period">pm</span>
-        </div>
+          <div class="time-footers one columns">
+            <span id="thursday-start-times">7:00</span><span id="thursday-start-period">am</span>
+          </div>
 
-        <div class="date-headers one columns end">
-          <span id="sunday-stop-times">7:00</span><span id="sunday-stop-period">pm</span>
-        </div>
-      </div><!-- end row -->
+          <div class="time-footers one columns">
+            <span id="friday-start-times">7:00</span><span id="friday-start-period">am</span>
+          </div>
 
-    </div><!-- end col -->
-  </div><!-- end row -->
+          <div class="time-footers one columns">
+            <span id="saturday-start-times">7:00</span><span id="saturday-start-period">am</span>
+          </div>
+
+          <div class="time-footers one columns end">
+            <span id="sunday-start-times">7:00</span><span id="sunday-start-period">am</span>
+          </div>
+        </div><!-- end row -->
+
+        <div class="row collapse">
+          <div class="time-footers one columns">
+            
+          </div>
+
+          <div class="time-footers one columns">
+            <span id="monday-stop-times">7:00</span><span id="monday-stop-period">pm</span>
+          </div>
+
+          <div class="time-footers one columns">
+            <span id="tuesday-stop-times">7:00</span><span id="tuesday-stop-period">pm</span>
+          </div>
+
+          <div class="time-footers one columns">
+            <span id="wednesday-stop-times">7:00</span><span id="wednesday-stop-period">pm</span>
+          </div>
+
+          <div class="time-footers one columns">
+            <span id="thursday-stop-times">7:00</span><span id="thursday-stop-period">pm</span>
+          </div>
+
+          <div class="time-footers one columns">
+            <span id="friday-stop-times">7:00</span><span id="friday-stop-period">pm</span>
+          </div>
+
+          <div class="time-footers one columns">
+            <span id="saturday-stop-times">7:00</span><span id="saturday-stop-period">pm</span>
+          </div>
+
+          <div class="time-footers one columns end">
+            <span id="sunday-stop-times">7:00</span><span id="sunday-stop-period">pm</span>
+          </div>
+        </div><!-- end row -->
+
+      </div><!-- end col -->
+    </div><!-- end row -->
+    <div class="schedule-submit row collapse">
+      <div class="twelve columns">
+          <input type="submit" name="submit" id="submit" class="button">
+      </div>
+    </div><!-- end row -->
+  </form>
   
   
   <!-- Included JS Files (Compressed) -->
