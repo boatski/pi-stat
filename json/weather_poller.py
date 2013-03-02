e00577c0ce5f91f585fcc6558d17223d6dc9c868
Chad Strauch <boatski313@gmail.com>
2013-03-02 00:34:30 -0500

    request json data from weather underground

---
+++ json/weather_poller.py
@@ -0,0 +1,15 @@
+import json
+import urllib2
+
+class WeatherPoller(object):
+
+	def __init__(self):
+		pass
+
+	"""
+	Requests the json object from weather underground, reads the response, then returns the json string.
+	"""
+	def pollWeather(self):
+		response = urllib2.urlopen('http://api.wunderground.com/api/3dcd75dc0e162700/conditions/q/IN/Indianapolis.json')
+		data = response.read()
+		return json.loads(data)
