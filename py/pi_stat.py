92badfafa7f1405cc7869fecaf412a65a0f14879
Chad Strauch <boatski313@gmail.com>
2013-03-02 21:50:12 -0500

    added call to update weather every 5 mins

--- py/pi_stat.py
+++ py/pi_stat.py
@@ -8,12 +8,16 @@ class PiStat(object):
     def __init__(self):
         # Time intervals for various functions in seconds
         self.sensorPollInterval = 5
+        self.weatherPollInterval = 300
 
         # Objects
         self.sensor = Sensor()
         self.tstat = Thermostat(self.sensor)
-        
-        # start calling f now and every 60 sec thereafter
+
+        # start calling pollWeather now and every 300 sec thereafter
+        self.pollWeather()
+
+        # start calling pollSensor now and every 5 sec thereafter
         self.pollSensor()
 
 
@@ -29,6 +33,16 @@ class PiStat(object):
 
         # call pollSensor() again in 'sensorPollInterval' seconds
         threading.Timer(self.sensorPollInterval, self.pollSensor).start()
+
+    """
+    Polls Underground Weather for the current conditions. City, State is determined
+    in weather_poller.py with the json link.
+    """
+    def pollWeather(self):
+        self.sensor.pollWeather()
+
+        # call pollWeather() again in 'weatherPollInterval' seconds
+        threading.Timer(self.weatherPollInterval, self.pollWeather).start()
 
 
 # Start the script
