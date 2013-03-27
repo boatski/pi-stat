107f559c4f167dd5a3b5577bd2e73b63824431df
Chad Strauch <boatski313@gmail.com>
2013-03-26 23:30:42 -0400

    www-data needs to run gpio with sudo

--- json/output_poller.py
+++ json/output_poller.py
@@ -13,17 +13,22 @@ class OutputPoller(object):
 		pass
 
 	"""
-	Reads the status of the three GPIO used as outputs.
+	Reads the status of the three GPIO used as outputs. Web server user (www-data) needs
+	to be able to run sudo on the gpio command without requiring a password.
 	"""
 	def updateOutputData(self):
-		process = subprocess.Popen([self.gpio, self.read, str(self.fanPin)], stdout=subprocess.PIPE)
-		fan, err = process.communicate()
+		try:
+			fan = subprocess.check_output(["sudo", "gpio", "read", "0"])
 
-		process = subprocess.Popen([self.gpio, self.read, str(self.heatPin)], stdout=subprocess.PIPE)
-		heat, err = process.communicate()
+			heat = subprocess.check_output(["sudo", "gpio", "read", "1"])
 
-		process = subprocess.Popen([self.gpio, self.read, str(self.coolPin)], stdout=subprocess.PIPE)
-		cool, err = process.communicate()
+			cool = subprocess.check_output(["sudo", "gpio", "read", "2"])
 
-		status = {'fan':fan, 'heat':heat, 'cool':cool}
+			status = {'fan':self.removeEscapeChar(fan), 'heat':self.removeEscapeChar(heat), 'cool':self.removeEscapeChar(cool)}
+		except Exception:
+			status = {'fan':"FAIL", 'heat':"FAIL", 'cool':"FAIL"}
+
 		return status
+
+	def removeEscapeChar(self, out):
+		return out[0]
